e1a71f7c07edad26b5e5c9547cde8475dc4fc141
Adam Patterson <hello@adampatterson.ca>
2014-12-21 11:09:19 -0700

    asset cleanup

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+// @codekit-prepend "../vendor/jquery/dist/jquery.min.js";
+// @codekit-prepend "../vendor/bootstrap/dist/js/bootstrap.min.js";
+
+// @codekit-append "../vendor/hover.zoom.js";
+// @codekit-append "../vendor/hover.zoom.conf.js";
