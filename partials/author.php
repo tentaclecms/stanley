4d7be09c1fc627f7cd5dce9c62858bca91efae54
Adam Patterson <hello@adampatterson.ca>
2015-01-03 15:51:54 -0700

    author udpate

--- partials/author.php
+++ partials/author.php
@@ -1,15 +1,10 @@
 <section class="author row m-top-45">
     <figure class="author-image col-sm-2 col-xs-4 col-xs-center col-sm-center md-center">
-        <img src="<?= THEME ?>/assets/img/user.png" width="100px" height="100px" class="hide-print img-circle profile-large">
+        <img src="<?= get_gravatar(get::option('admin_email'), 100, 100) ?>" alt="<?= get::option('admin_author') ?>" class="img-circle"/>
     </figure>
     <section class="author">
-        <h4>Adam Patterson</h4>
-        <p>User Interface Designer & Developer with a background in UX. I have spent 5 years as a professionally certified bicycle mechanic and ride year-round.</p>
-        <p>I am a husband and father of two, I enjoy photography, music, movies, coffee, and good food.</p>
-        <p>You can find me on Twitter <a href="http://twitter.com/adampatterson">@adampatterson</a> and <a href="http://instagram.com/adamapatterson">Instagram</a></p>
-    </section>
-    <section class="share">
-        &nbsp;
+        <h4>Hi, I am <?= get::option('admin_author') ?>!</h4>
+        <?= the_content(snippet('hero_about')) ?>
     </section>
 </section>
 
